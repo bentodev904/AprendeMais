255132d6b7c423cc5f23865737d9643e283172ca
BentoBento <148278977+bentodev904@users.noreply.github.com>
2024-09-18 19:11:00 +0000

    hhh

--- scripts/matematica.js
+++ scripts/matematica.js
@@ -3,4 +3,16 @@ function troca(container) {
         document.getElementById("all").style = "display:none;"
         document.getElementById("imc").style = "display:block;"
     }
+    if (container == 2) {
+        document.getElementById("all").style = "display:none;"
+        document.getElementById("conversor").style = "display:block;"
+    }
+    if (container == 3) {
+        document.getElementById("all").style = "display:none;"
+        document.getElementById("gastar").style = "display:block;"
+    }
+    if (container == 4) {
+        document.getElementById("all").style = "display:none;"
+        document.getElementById("sortear").style = "display:block;"
+    }
 }
